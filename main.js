f1e3b41f7a3853245783ae72c63086a34ca90f68
junki.hiroi <junki.hiroi@drecom.co.jp>
2021-01-13 13:46:01 +0900

    扱いにくかったので前面に固定しないように変更

--- main.js
+++ main.js
@@ -116,7 +116,6 @@ function createMainWindow()
         webPreferences: {
             nodeIntegration: true
         },
-        alwaysOnTop: true,
     });
     mainWindow.loadURL('file://' + __dirname + '/lib/html/main_viewer.html');
     mainWindow.center();
